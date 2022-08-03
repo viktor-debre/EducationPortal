2339c618182e8ed50f07d27cac7a08514d7a958f
viktor-debre <viktor.debre2002@gmail.com>
2022-08-03 14:50:03 +0300

    Add IRepository interface to interact with data storage

---
+++ Education Portal/EducationPortal/EducationPortal.Application/Interfaces/Repository/IRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace EducationPortal.Application.Interfaces.Repository
+{
+    public interface IRepository<T>
+    {
+        public void AddItemToStorage(List<T> item, string path);
+
+        public List<T> ExctractItemsFromStorage(string path);
+    }
+}
