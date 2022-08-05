2120f76cb84cded658aa4dddbf2c1db302b31bda
viktor-debre <viktor.debre2002@gmail.com>
2022-08-05 14:39:01 +0300

    Add file context to provide access to files storage

---
+++ Education Portal/EducationPortal/EducationPortal.Infrastructure/StorageService/FileContext.cs
@@ -0,0 +1,25 @@
+﻿using EducationPortal.Application.Interfaces.Shared;
+using EducationPortal.Domain.Entities;
+
+namespace EducationPortal.Infrastructure.StorageService
+{
+    public class FileContext : IUserCRUD
+    {
+        public List<User>? Users { get; set; }
+        StorageManager<User> storage = new StorageManager<User>();
+
+        public List<User> ReadUserFromStorage()
+        {
+            string userPath = @"D:\work\users.json";
+            Users = storage.ExctractItemsFromStorage(userPath);
+            return Users;
+        }
+
+        public void SetUserInStorage(User user)
+        {
+            string userPath = @"D:\work\users.json";
+            Users.Add(user);
+            storage.AddItemToStorage(Users, userPath);
+        }
+    }
+}
--- Education Portal/EducationPortal/EducationPortal.Infrastructure/StorageService/StorageManager.cs
+++ Education Portal/EducationPortal/EducationPortal.Infrastructure/StorageService/StorageManager.cs
@@ -6,24 +6,20 @@ namespace EducationPortal.Infrastructure.StorageService
 {
     internal class StorageManager<T> : IRepository<T>
     {
-        static string defaultPath = @"D:\work\result.json";
         JsonSerializer<T> jsonItems = new JsonSerializer<T>();
         FileManager fileManager = new FileManager();
 
         public void AddItemToStorage(List<T> item, string path)
         {
-            path = defaultPath;
             string jsonString = jsonItems.SerializeItem(item);
             fileManager.WriteFile(path, jsonString);
         }
 
         public List<T> ExctractItemsFromStorage(string path)
         {
-            path = defaultPath;
             string readed = fileManager.ReadFile(path);
             List<T> deserialized = jsonItems.DecerializeItem(readed);
             return deserialized;
         }
-
     }
 }
