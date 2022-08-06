9b643286a6850dd1664567f77f2204f6c4fb2dd7
viktor-debre <viktor.debre2002@gmail.com>
2022-08-06 22:55:15 +0300

    Fix naming in course property

--- Education Portal/EducationPortal/EducationPortal.Domain/Entities/Course.cs
+++ Education Portal/EducationPortal/EducationPortal.Domain/Entities/Course.cs
@@ -1,9 +1,11 @@
-﻿namespace EducationPortal.Domain.Entities
+﻿using EducationPortal.Domain.Entities.Materials;
+
+namespace EducationPortal.Domain.Entities
 {
     public class Course
     {
         string Name { get; set; }
         string Description { get; set; }
-        List<Course> CourseList { get; set; }
+        List<Material> Matherials { get; set; }
     }
 }
