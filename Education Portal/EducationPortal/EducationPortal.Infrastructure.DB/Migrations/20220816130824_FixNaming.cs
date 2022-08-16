187fe4497610470c5ed33c6ca22912aa2102a6a0
viktor-debre <viktor.debre2002@gmail.com>
2022-08-16 16:47:37 +0300

    Add migration for fixing naming of tables in database

---
+++ Education Portal/EducationPortal/EducationPortal.Infrastructure.DB/Migrations/20220816130824_FixNaming.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EducationPortal.Infrastructure.DB.Migrations
+{
+    public partial class FixNaming : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DbCourseDbMaterial_DbCourse_CoursesId",
+                table: "CourseMaterial");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_DbCourse",
+                table: "Course");
+
+            migrationBuilder.RenameTable(
+                name: "Course",
+                newName: "Courses");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Courses",
+                table: "Courses",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DbCourseDbMaterial_Courses_CoursesId",
+                table: "CourseMaterial",
+                column: "CoursesId",
+                principalTable: "Courses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DbCourseDbMaterial_Courses_CoursesId",
+                table: "CourseMaterial");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Courses",
+                table: "Courses");
+
+            migrationBuilder.RenameTable(
+                name: "Courses",
+                newName: "Course");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_DbCourse",
+                table: "Course",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DbCourseDbMaterial_DbCourse_CoursesId",
+                table: "CourseMaterial",
+                column: "CoursesId",
+                principalTable: "Course",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
