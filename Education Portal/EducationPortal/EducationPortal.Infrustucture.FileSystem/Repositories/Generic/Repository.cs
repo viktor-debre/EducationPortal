5df5f89b50f54040695912a80153fb81a99597dc
viktor-debre <viktor.debre2002@gmail.com>
2022-09-03 22:59:29 +0300

    Make repository generic for file system

---
+++ Education Portal/EducationPortal/EducationPortal.Infrustucture.FileSystem/Repositories/Generic/Repository.cs
@@ -0,0 +1,79 @@
+﻿using EducationPortal.Domain.Common;
+using EducationPortal.Domain.Repository;
+using System.Reflection;
+
+namespace EducationPortal.Infrustucture.FileSystem.Repositories.Generic
+{
+    internal class Repository<TEntity> : IRepository<TEntity> where TEntity : BaseEntity
+    {
+        private readonly StorageManager<TEntity> _storage = new StorageManager<TEntity>();
+
+        public Repository()
+        {
+            PathToEntity = GetPathToEntity();
+            EntitiesList = new List<TEntity>();
+        }
+
+        public string PathToEntity { get; set; }
+
+        public List<TEntity> EntitiesList { get; set; }
+
+        public void Add(TEntity item)
+        {
+            EntitiesList.Add(item);
+            _storage.AddItemToStorage(EntitiesList, PathToEntity);
+        }
+
+        public List<TEntity> Find()
+        {
+            List<TEntity> materials = _storage.ExctractItemsFromStorage(PathToEntity);
+            if (materials != null)
+            {
+                EntitiesList = materials;
+            }
+
+            return EntitiesList;
+        }
+
+        public TEntity? FindById(int id)
+        {
+            return EntitiesList.FirstOrDefault(x => x.Id == id);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            var item = FindById(entity.Id);
+            if (item != null)
+            {
+                EntitiesList.Remove(item);
+                _storage.AddItemToStorage(EntitiesList, PathToEntity);
+            }
+        }
+
+        public void Update(TEntity item)
+        {
+            TEntity? oldItem = FindById(item.Id);
+            if (oldItem != null)
+            {
+                Remove(oldItem);
+                Add(item);
+            }
+        }
+
+        private string GetPathToEntity()
+        {
+            string workingDirectory = Environment.CurrentDirectory;
+            string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.Parent.FullName;
+            string fileStorageLevel = @"\EducationPortal.Infrustucture.FileSystem\JsonFiles\";
+            Type type = typeof(TEntity);
+            string nameEntity = type.Name;
+            string pathToEntityStorage = projectDirectory + fileStorageLevel + nameEntity + ".json";
+            if (!File.Exists(pathToEntityStorage))
+            {
+                File.Create(pathToEntityStorage);
+            }
+            
+            return pathToEntityStorage;
+        }
+    }
+}
