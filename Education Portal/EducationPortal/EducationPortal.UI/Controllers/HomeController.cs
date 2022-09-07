ec183ff8dc4dc97105e6cb7b904acb73ab577cc9
viktor-debre <viktor.debre2002@gmail.com>
2022-09-07 12:18:52 +0300

    Add new controller UserInfo for user profile

--- Education Portal/EducationPortal/EducationPortal.UI/Controllers/HomeController.cs
+++ Education Portal/EducationPortal/EducationPortal.UI/Controllers/HomeController.cs
@@ -7,15 +7,17 @@ namespace EducationPortal.UI.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUserInformationService _userInformation;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUserInformationService userInformation)
         {
             _logger = logger;
+            _userInformation = userInformation;
         }
 
         public IActionResult Index()
         {
-            return View();
+            return View(_userInformation.GetUserInfo("Viktor"));
         }
 
         public IActionResult Privacy()
@@ -27,6 +29,11 @@ namespace EducationPortal.UI.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult UserProfile()
+        {
+            return View(_userInformation.GetUserInfo("Viktor"));
         }
     }
 }
---
+++ Education Portal/EducationPortal/EducationPortal.UI/Controllers/UserInfoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EducationPortal.UI.Controllers
+{
+    internal class UserInfoController : Controller
+    {
+        private readonly IUserInformationService _userInformation;
+
+        public UserInfoController(IUserInformationService userInformation)
+        {
+            _userInformation = userInformation;
+        }
+
+        public IActionResult UserProfile()
+        {
+            return View(_userInformation.GetUserInfo("Viktor"));
+        }
+    }
+}
