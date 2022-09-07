b8e7c22908af2cbae2463afbc9ccc9b38ab70805
viktor-debre <viktor.debre2002@gmail.com>
2022-09-07 12:17:51 +0300

    Add dependency injection for ui services and mapping

---
+++ Education Portal/EducationPortal/EducationPortal.UI/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using EducationPortal.UI.Models.Mapping;
+using EducationPortal.UI.Services.Implementation;
+
+namespace EducationPortal.UI
+{
+    public static class DependencyInjection
+    {
+        public static void RegisterUIServices(IServiceCollection services)
+        {
+            services.AddSingleton<IUserInformationService, UserInformationService>()
+                .AddSingleton<IMapper, MapperForViewModels>();
+        }
+    }
+}
