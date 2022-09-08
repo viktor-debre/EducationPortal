a2f337efe414d38d4c78f781b9512c00703f6ef1
viktor-debre <viktor.debre2002@gmail.com>
2022-09-08 22:43:38 +0300

    Add command and validation for skill creating

---
+++ Education Portal/EducationPortal/EducationPortal.Application/Commands/Validation/CreateSkillValidation.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace EducationPortal.Application.Commands.Validation
+{
+    internal class CreateSkillValidation : AbstractValidator<Skill>
+    {
+        public CreateSkillValidation()
+        {
+            RuleFor(u => u.Title)
+               .NotEmpty();
+        }
+    }
+}
