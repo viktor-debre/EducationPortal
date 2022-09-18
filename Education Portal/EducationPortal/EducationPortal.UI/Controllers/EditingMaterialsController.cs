9c3d50d45d0d0394daf48fb51264060c1cb54cce
viktor-debre <viktor.debre2002@gmail.com>
2022-09-18 21:31:07 +0300

    Add methods for CRUD to controller

--- Education Portal/EducationPortal/EducationPortal.UI/Controllers/EditingMaterialsController.cs
+++ Education Portal/EducationPortal/EducationPortal.UI/Controllers/EditingMaterialsController.cs
@@ -19,19 +19,19 @@ namespace EducationPortal.UI.Controllers
             return View(materials);
         }
 
-        public IActionResult Create()
+        public IActionResult CreateArticle()
         {
             return View();
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create(MaterialView material)
+        public async Task<IActionResult> CreateArticle(ArticleView material)
         {
             if (ModelState.IsValid)
             {
                 if (material.Id == 0)
                 {
-                    await _materialService.SetMaterial(material);
+                    await _materialService.SetArticle(material);
                 }
 
                 return RedirectToAction("Materials");
@@ -40,11 +40,85 @@ namespace EducationPortal.UI.Controllers
             return View();
         }
 
-        public async Task<IActionResult> Edit(int? id)
+        public IActionResult CreateBook()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBook(BookView material)
         {
             if (ModelState.IsValid)
             {
-                MaterialView? material = await _materialService.GetByIdMaterial(id ?? 0);
+                if (material.Id == 0)
+                {
+                    await _materialService.SetBook(material);
+                }
+
+                return RedirectToAction("Materials");
+            }
+
+            return View();
+        }
+
+        public IActionResult CreateVideo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateVideo(VideoView material)
+        {
+            if (ModelState.IsValid)
+            {
+                if (material.Id == 0)
+                {
+                    await _materialService.SetVideo(material);
+                }
+
+                return RedirectToAction("Materials");
+            }
+
+            return View();
+        }
+
+        public async Task<IActionResult> EditArticle(int? id)
+        {
+            if (ModelState.IsValid)
+            {
+                ArticleView? material = await _materialService.GetByIdArticle(id ?? 0);
+                if (material != null)
+                {
+                    return View(material);
+                }
+
+                return NotFound();
+            }
+
+            return View(id);
+        }
+
+        public async Task<IActionResult> EditBook(int? id)
+        {
+            if (ModelState.IsValid)
+            {
+                BookView? material = await _materialService.GetByIdBook(id ?? 0);
+                if (material != null)
+                {
+                    return View(material);
+                }
+
+                return NotFound();
+            }
+
+            return View(id);
+        }
+
+        public async Task<IActionResult> EditVideo(int? id)
+        {
+            if (ModelState.IsValid)
+            {
+                VideoView? material = await _materialService.GetByIdVideo(id ?? 0);
                 if (material != null)
                 {
                     return View(material);
@@ -57,20 +131,59 @@ namespace EducationPortal.UI.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Edit(MaterialView material)
+        public async Task<IActionResult> EditArticle(ArticleView material)
         {
-            await _materialService.UpdateMaterial(material);
+            await _materialService.UpdateArticle(material);
 
             return RedirectToAction("Materials");
         }
 
         [HttpPost]
-        public async Task<IActionResult> Delete(int? id)
+        public async Task<IActionResult> EditBook(BookView material)
         {
-            MaterialView? material = await _materialService.GetByIdMaterial(id ?? 0);
+            await _materialService.UpdateBook(material);
+
+            return RedirectToAction("Materials");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditVideo(VideoView material)
+        {
+            await _materialService.UpdateVideo(material);
+
+            return RedirectToAction("Materials");
+        }
+
+        public async Task<IActionResult> DeleteArticle(int? id)
+        {
+            ArticleView? material = await _materialService.GetByIdArticle(id ?? 0);
             if (material != null)
             {
-                _materialService.RemoveMaterial(material);
+                _materialService.RemoveArticle(material);
+                return RedirectToAction("Materials");
+            }
+
+            return NotFound();
+        }
+
+        public async Task<IActionResult> DeleteBook(int? id)
+        {
+            BookView? material = await _materialService.GetByIdBook(id ?? 0);
+            if (material != null)
+            {
+                _materialService.RemoveBook(material);
+                return RedirectToAction("Materials");
+            }
+
+            return NotFound();
+        }
+
+        public async Task<IActionResult> DeleteVideo(int? id)
+        {
+            VideoView? material = await _materialService.GetByIdVideo(id ?? 0);
+            if (material != null)
+            {
+                _materialService.RemoveVideo(material);
                 return RedirectToAction("Materials");
             }
 
