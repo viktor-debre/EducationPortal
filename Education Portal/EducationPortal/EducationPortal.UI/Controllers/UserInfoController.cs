f10252c0fcc4721d30a6981aa7e26a46472df1ee
viktor-debre <viktor.debre2002@gmail.com>
2022-09-20 18:07:23 +0300

    Add to user profile information about skills level and courses status

--- Education Portal/EducationPortal/EducationPortal.UI/Controllers/UserInfoController.cs
+++ Education Portal/EducationPortal/EducationPortal.UI/Controllers/UserInfoController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using EducationPortal.Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EducationPortal.UI.Controllers
@@ -16,7 +17,32 @@ namespace EducationPortal.UI.Controllers
         [HttpGet]
         public async Task<IActionResult> UserProfile()
         {
-            ViewBag.AuthorizedUser = await _userInformation.GetUserInfo(User.Identity.Name);
+            var user = await _userInformation.GetUserInfo(User.Identity.Name);
+            ViewBag.AuthorizedUser = user;
+            var userSkills = user.UserSkills;
+            List<SkillView> skills = new List<SkillView>();
+            for (int i = 0; i < userSkills.Count; ++i)
+            {
+                SkillView? findedSkill = user.Skills.FirstOrDefault(x => x.Id == userSkills[i].SkillId);
+                if (findedSkill != null)
+                {
+                    skills.Add(findedSkill);
+                }
+            }
+
+            var userCourses = user.UserCourses;
+            List<CourseView> courses = new List<CourseView>();
+            for (int i = 0; i < userCourses.Count; ++i)
+            {
+                CourseView? findedCourse = user.Courses.FirstOrDefault(x => x.Id == userCourses[i].CourseId);
+                if (findedCourse != null)
+                {
+                    courses.Add(findedCourse);
+                }
+            }
+
+            ViewBag.UserSkills = skills;
+            ViewBag.UserCourses = courses;
             return View();
         }
     }
