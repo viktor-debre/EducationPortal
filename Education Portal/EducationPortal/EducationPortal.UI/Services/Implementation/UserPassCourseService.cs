a99cd8157379752e01304143f69b509ebdbbf3e9
viktor-debre <viktor.debre2002@gmail.com>
2022-09-20 20:55:38 +0300

    Add services for passing course

---
+++ Education Portal/EducationPortal/EducationPortal.UI/Services/Implementation/UserPassCourseService.cs
@@ -0,0 +1,65 @@
+﻿using EducationPortal.Application.Interfaces.Shared;
+using EducationPortal.UI.Models.Mapping;
+
+namespace EducationPortal.UI.Services.Implementation
+{
+    public class UserPassCourseService : IUserPassCourseService
+    {
+        private readonly IUserCourseService _userCourse;
+        private readonly IMapper _mapper;
+
+        public UserPassCourseService(IUserCourseService userCourse, IMapper mapper)
+        {
+            _userCourse = userCourse;
+            _mapper = mapper;
+        }
+
+        public async Task<List<CourseView>> GetAvailableCourses(int userId)
+        {
+            List<CourseView> courses = new List<CourseView>();
+            foreach (var course in await _userCourse.GetAvailableCourses(userId))
+            {
+                courses.Add(_mapper.MapCourseToViewModel(course));
+            }
+
+            return courses;
+        }
+
+        public async Task<List<UserCourseView>> GetPassedCourses(int userId)
+        {
+            List<UserCourseView> courses = new List<UserCourseView>();
+            foreach (var course in await _userCourse.GetPassedCourses(userId))
+            {
+                courses.Add(_mapper.MapUserCourseToViewModel(course));
+            }
+
+            return courses;
+        }
+
+        public async Task<List<UserCourseView>> GetStartedCourses(int userId)
+        {
+            List<UserCourseView> courses = new List<UserCourseView>();
+            foreach (var course in await _userCourse.GetStartedCourses(userId))
+            {
+                courses.Add(_mapper.MapUserCourseToViewModel(course));
+            }
+
+            return courses;
+        }
+
+        public async Task<UserCourseView> GetUserCoursesById(int userId, int courseId)
+        {
+            return _mapper.MapUserCourseToViewModel(await _userCourse.GetUserCoursesById(userId, courseId));
+        }
+
+        public async Task<bool> PassMaterial(CourseView course, string nameMaterial, int userId)
+        {
+            return await _userCourse.PassMaterial(_mapper.MapCourseToDomainModel(course), nameMaterial, userId);
+        }
+
+        public async Task TakeCourse(CourseView course, int userId)
+        {
+            await _userCourse.TakeCourse(_mapper.MapCourseToDomainModel(course), userId);
+        }
+    }
+}
---
+++ Education Portal/EducationPortal/EducationPortal.UI/Services/Interfaces/IUserPassCourseService.cs
@@ -0,0 +1,17 @@
+﻿namespace EducationPortal.UI.Services.Interfaces
+{
+    public interface IUserPassCourseService
+    {
+        public Task<UserCourseView> GetUserCoursesById(int userId, int courseId);
+
+        public Task<List<CourseView>> GetAvailableCourses(int userId);
+
+        public Task<List<UserCourseView>> GetStartedCourses(int userId);
+
+        public Task<List<UserCourseView>> GetPassedCourses(int userId);
+
+        public Task TakeCourse(CourseView course, int userId);
+
+        public Task<bool> PassMaterial(CourseView course, string nameMaterial, int userId);
+    }
+}
