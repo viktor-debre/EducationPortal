8605946f07138be300c4a619144ea5194468896f
viktor-debre <viktor.debre2002@gmail.com>
2022-12-14 16:57:51 +0200

    Create class for User Service Testing

---
+++ Education Portal/EducationPortal/EducationPortal.Application.Test/Services/UserServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using EducationPortal.Application.Interfaces.Shared;
+
+namespace EducationPortal.Application.Test.Services
+{
+    public class UserServiceTest
+    {
+        private readonly IUserService _userService;
+
+        public UserServiceTest(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [Fact]
+        public async Task AuthenticateWithNotExistingUser()
+        {
+            Assert.Equal(await _userService.Authenticate("vebro", "1"), 0);
+        }
+    }
+}
